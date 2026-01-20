5242f3c7184a9bbe275ae4e48a2341e2012fbed4
Jayamurugan15 <sjmurugan.1505@gmail.com>
2026-01-20 08:44:00 +0530

    Uncontrolled without Ref

--- ControlledUncontrolled/src/App.jsx
+++ ControlledUncontrolled/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Contact from './Components/ControlledContactForm'
 import UncontrolledWithRef from './Components/UncontrolledWithRef'
+import UncontrolledWithoutRef from './Components/UncontrolledWithoutRef'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,8 @@ function App() {
   return (
     <>
       {/* <Contact/> */}
-      <UncontrolledWithRef/>
+      {/* <UncontrolledWithRef/> */}
+      <UncontrolledWithoutRef/>
     </>
   )
 }
--- ControlledUncontrolled/src/Components/UncontrolledWithoutRef.jsx
+++ ControlledUncontrolled/src/Components/UncontrolledWithoutRef.jsx
@@ -1,85 +1,46 @@
-import React, { useRef, useState } from 'react'
+import React from 'react'
 
-const UncontrolledWithRef = ( ) => {
+const UncontrolledWithoutRef = () => {
 
-    const nameRef = useRef("");
-    const emailRef = useRef("");
-    const phoneRef = useRef("");
-    const messageRef =  useRef("");
-    
-      const handlechange = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        
-      }
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
+        const formData = new FormData(e.target);
+        const data = Object.fromEntries(formData.entries());
+        console.log("Form Data from Without ref : >>>>",data)
+    }
 
-        const name = nameRef.current.value;
-        const phone = phoneRef.current.value;
-        const email = emailRef.current.value;
-        const message = messageRef.current.value;
-
-        if(!name) {
-          nameRef.current.focus();
-          return
-        }
-
-        if(!/^\d{10}$/.test(phone)){
-          phoneRef.current.focus();
-          return;
-        }
-
-        if(!email.includes("@")){
-          emailRef.current.focus();
-          return;
-        }
-
-        if(!message){
-          messageRef.current.focus();
-          return;
-        }
-       
-
-        console.log("Form Submitted :", {name,email,phone,message})
-      }
   return (
-     <div className="container">
+    <div className="container">
           <div className="flex flex-wrap justify-center mt-5">
             <div className="w-full px-4 lg:w-1/2 xl:w-5/12">
               <div className="rounded-lg bg-white p-8 shadow-lg sm:p-12">
-                <h1 className="text-center text-red-500 font-bold mb-4">UnControlled Component</h1>
+                <h1 className="text-center text-green-500 font-bold mb-4">UnControlled Component Without Ref</h1>
                 <form
                 className="flex flex-col gap-3" 
                 onSubmit={handleSubmit}>
                   <input
                     type="text"
                     name="name"
-                    ref={nameRef}
                     placeholder="Your Name"
                     className="w-full rounded border border-stroke px-3.5 py-3 text-base outline-none focus:border-2 focus:border-blue-500"
                   />
                   <input
-                    ref={emailRef}
                     type="email"
                     name="email"
-                    onChange={handlechange}
                     placeholder="Your Email"
                     className="w-full rounded border border-stroke px-3.5 py-3 text-base outline-none focus:border-2 focus:border-blue-500"
                   />
                   <input
-                    ref={phoneRef}
+                    
                     type="tel"
                     name="phone"
-                    onChange={handlechange}
                     placeholder="Your Phone"
                     className="w-full rounded border border-stroke px-3.5 py-3 text-base outline-none focus:border-2 focus:border-blue-500"
                   />
                   <div className="mb-6">
                     <textarea
-                      ref={messageRef}
+                
                       rows={2}
-                      onChange={handlechange}
                       placeholder="Your Message"
                       name="message"
                       className="w-full resize-none rounded border border-stroke px-3.5  py-3 text-base text-body-color outline-none focus:border-2 focus:border-blue-500"
@@ -102,4 +63,4 @@ const UncontrolledWithRef = ( ) => {
   )
 }
 
-export default UncontrolledWithRef
+export default UncontrolledWithoutRef
