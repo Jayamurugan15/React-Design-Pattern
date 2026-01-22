7c7f5e29724273dea111914a79282f6183d752e9
Jayamurugan15 <sjmurugan.1505@gmail.com>
2026-01-22 07:27:32 +0530

    Props Drill Done

--- Day03/src/App.jsx
+++ Day03/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Modal from "./Components/Modal";
 import ModalContainer from "./Components/ModalContainer";
-import Card from "./Components/Card";
+import Card from "./Components/MessyComponent/Card";
 
 function App() {
   
@@ -9,7 +9,21 @@ function App() {
   return (
     <>
       {/* <ModalContainer/> */}
-      <Card/>
+      <Card
+      name={"Jayamurugan"}
+      designation={"Web Developer"}
+      experience={"1"}
+      projects={8}
+      ctc={"2"}
+      about={`Web Developer skilled in building responsive, component-based web applications using React.js, Redux Toolkit,
+      JavaScript (ES6+),TypeScript and Tailwind CSS. Proficient in translating UI/UX designs into clean, maintainable code with
+      seamless API integrations. Continuously enhancing technical expertise through real-world project development`}
+      LinkedIn={"https://www.linkedin.com/in/jayamurugans/"}
+      location={"Chennai"}
+      role={"Web Developer @ Looperex"}
+      openToWork={true}
+      skills={["Next.js","TypeScript","Javascript","React.js","Redux","Node.js","Express.js","Tailwindcss"]}
+      image={"https://images.unsplash.com/photo-1560250097-0b93528c311a?w=400"}/>
     </>
   );
 }
