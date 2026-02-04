60ab48255a41373ae36781d3fc37af97467ab415
Jayamurugan15 <sjmurugan.1505@gmail.com>
2026-02-04 20:56:01 +0530

    initial commit

---
+++ HOCPattern/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-2xl font-semibold text-green-500'>Higher Order Components</h1>
+    </>
+  )
+}
+
+export default App
